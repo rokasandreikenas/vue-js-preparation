12acdf701d887389ce09d56e8380f6a237496e8c
rokasandreikenas <shyptt@gmail.com>
2023-07-12 17:41:09 +0300

    Add counter app

---
+++ counter/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import CounterComponent from './components/CounterComponent.vue'
+</script>
+
+<template>
+  <header>
+    <nav class="navigation">
+      <a>One</a>
+      <a>Two</a>
+      <a>Three</a>
+    </nav>
+  </header>
+  <main>
+    <CounterComponent />
+  </main>
+</template>
+
+<style>
+.navigation {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+}
+</style>
---
+++ counter/src/components/CounterComponent.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+const increment = () => count.value++
+const decrement = () => count.value--
+</script>
+
+<template>
+  <div class="counter">
+    <h1>Counter</h1>
+    <h2 class="count">{{ count }}</h2>
+    <div class="buttons">
+      <button @click="increment" :disabled="count === 3">+1</button>
+      <button @click="decrement" :disabled="count === -3">-1</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.counter {
+  width: 1100px;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 80px;
+}
+
+.counter .count {
+  margin: 40px 0;
+}
+
+.counter .buttons {
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  font-size: 24px;
+}
+
+button {
+  padding: 10px 14px;
+}
+</style>
