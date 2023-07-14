a64bbb0d54123b8eabca1bd00a846634931b5f12
rokasandreikenas <shyptt@gmail.com>
2023-07-14 19:51:22 +0300

    Update UserCard when isSelected

--- forum/src/components/user/UserCard.vue
+++ forum/src/components/user/UserCard.vue
@@ -4,6 +4,7 @@ import type { User } from '../../types/user'
 
 interface Props {
   user: User
+  isSelected: boolean
   selectUser: (user: User) => void
 }
 
@@ -15,8 +16,21 @@ onMounted(() => {
 </script>
 
 <template>
-  <div @click="selectUser(user)">
-    <h2>{{ user.name }}</h2>
+  <div @click="selectUser(user)" class="card" :class="{ active: isSelected }">
+    <h2 :style="{ textDecoration: isSelected ? 'underline' : 'none' }">{{ user.name }}</h2>
     <p>{{ user.email }}</p>
   </div>
 </template>
+
+<style scoped>
+.card {
+  box-shadow: 2px 5px 10px 1px rgba(156, 155, 155, 0.34);
+  margin-bottom: 1rem;
+  padding: 1rem;
+  border-radius: 4px;
+}
+
+.active {
+  box-shadow: 2px 5px 10px 1px rgba(17, 103, 222, 0.34);
+}
+</style>
